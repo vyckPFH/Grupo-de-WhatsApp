73886b2deca75e64b7ef785ffe603ddaee3e77fa
vyckPFH <victoria.c.oliveira08@gmail.com>
2026-04-16 15:30:45 -0300

    abuu

--- crienteEnv.py
+++ crienteEnv.py
@@ -1,20 +1,32 @@
 import socket
 
-# HOST = "192.168.248.123"
 HOST = "192.168.247.135"
 PORT = 9002
 
-mensagem = input("[Cliente] Nickname: ")
+nickname = input("Nickname: ")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
     cliente.connect((HOST, PORT))
-    cliente.sendall(mensagem.encode("utf-8"))
 
+    # envia nickname
+    cliente.sendall(nickname.encode("utf-8"))
+
+    # espera confirmação de conexão
     resposta = cliente.recv(1024).decode("utf-8")
     print(f"[Server] {resposta}")
+
     while True:
         msg = input("Mensagem: ")
+
+        if not msg:
+            continue
+
         cliente.sendall(msg.encode("utf-8"))
-        # "visto" do whatsapp
+
+        # espera confirmação do servidor
         resposta = cliente.recv(1024).decode("utf-8")
-        print(resposta)
+
+        if resposta != "OK":
+            print("[ERRO] servidor não confirmou recebimento")
+        else:
+            print("enviado com sucesso")
