93bab5c292aea6b3e2cda4422117bf9243bb1ceb
vyckPFH <victoria.c.oliveira08@gmail.com>
2026-04-16 14:59:05 -0300

    novo patch

--- cliente_recv.py
+++ cliente_recv.py
@@ -3,10 +3,21 @@ import socket
 HOST = "192.168.247.135"
 PORT = 9003
 
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
     cliente.connect((HOST, PORT))
-    # conn = cliente.accept()
+
+    print("Conectado ao servidor de mensagens")
+
     while True:
-        resposta = cliente.recv(1024).decode("utf-8")
-        print(f"[Server] {resposta}")
+        try:
+            data = cliente.recv(1024)
+
+            if not data:
+                print("Servidor desconectou")
+                break
+
+            print(f"[Server] {data.decode('utf-8')}")
+
+        except Exception as e:
+            print(f"Erro: {e}")
+            break
